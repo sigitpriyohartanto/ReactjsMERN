a3aef28528d2168def7cb3badb61607dda6ed9b2
sigitpriyohartanto <sigith8@gmail.com>
2020-07-11 05:40:15 +0700

    Menambahkan fail Categories.js di folder parts dan image di folder public

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
 
 import landingPage from "json/landingPage.json";
 export default class LandingPage extends Component {
@@ -11,7 +12,6 @@ export default class LandingPage extends Component {
     this.refMostPicked = React.createRef();
   }
   render() {
-    console.log(this.props);
     return (
       <>
         <Header {...this.props}></Header>
@@ -20,6 +20,7 @@ export default class LandingPage extends Component {
           refMostPicked={this.refMostPicked}
           data={landingPage.mostPicked}
         />
+        <Categories data={landingPage.categories} />
       </>
     );
   }
